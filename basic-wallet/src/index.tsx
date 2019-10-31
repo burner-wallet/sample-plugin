ccf723bad0ad22386e56d4a3716640eea72cd4b0
David Mihal <david@dmihal.com>
2019-10-31 08:12:00 -0700

    Change basic-wallet to use Rinkeby

--- basic-wallet/src/index.tsx
+++ basic-wallet/src/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { xdai, dai, eth } from '@burner-wallet/assets';
+import { NativeAsset, ERC20Asset } from '@burner-wallet/assets';
 import BurnerCore from '@burner-wallet/core';
 import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers';
 import { InfuraGateway, InjectedGateway, XDaiGateway, } from '@burner-wallet/core/gateways';
 import Exchange, { Uniswap, XDaiBridge } from '@burner-wallet/exchange';
 import ModernUI from '@burner-wallet/modern-ui';
 import MyPlugin from 'my-plugin';
+
+const rinkebyETH = new NativeAsset({
+  id: 'reth',
+  name: 'rETH',
+  network: '4',
+});
+const rinkebyToken = new ERC20Asset({
+  id: 'rinkebyToken',
+  name: 'Rinkeby Token',
+  network: '4',
+  address: '0x1212121212121212121212121212121212121212', // Enter your token address here
+});
 
 const core = new BurnerCore({
   signers: [new InjectedSigner(), new LocalSigner()],
@@ -15,18 +27,14 @@ const core = new BurnerCore({
     new InfuraGateway(process.env.REACT_APP_INFURA_KEY),
     new XDaiGateway(),
   ],
-  assets: [xdai, dai, eth],
-});
-
-const exchange = new Exchange({
-  pairs: [new XDaiBridge(), new Uniswap('dai')],
+  assets: [rinkebyETH, rinkebyToken],
 });
 
 const BurnerWallet = () =>
   <ModernUI
     title="Basic Wallet"
     core={core}
-    plugins={[exchange, new MyPlugin()]}
+    plugins={[new MyPlugin()]}
   />
 
 
